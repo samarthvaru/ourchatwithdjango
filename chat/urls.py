095981c36ae3687df897d4e065c09b58ef5256fb
samarthvaru <samarthvaru111@gmail.com>
2020-05-22 13:59:05 +0530

    added consumers and routing websocket url

--- chat/urls.py
+++ chat/urls.py
@@ -3,6 +3,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('chat/<str:room_name>/', views.room, name='room')
 ]
